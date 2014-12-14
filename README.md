7d21b1e8ae0f9d33a9a2fda8323cb17d7152e2c8
Jake Rosoman <jkroso@gmail.com>
2014-12-14 17:44:50 +1300

    intro docs

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
-sunz.nz
-=======
+# sunz.nz
 
 Sunz Website
+
+## Navigating the project
+
+[app.js](app.js) is the HTTP server. Most of the code in it is just configuration crap though hopefully you can pick out where the request handlers are defined. It responds to `GET "/"` requests by rendering [views/index.jade](views/index.jade) ([jade](http://jade-lang.com) is just being used as a syntax layer over HTML in this case). This sends all the HTML to the browser in one go. As the browser parses it it will make requests for the [images](public/images), [javascript](public/js/index.js), and [css](public/stylesheets/style.css). Conceptually the server responds to these requests by just sending the corresponding file so you don't really need to know much there. However assuming this is the case you would expect the code to be super simple so I'll explain why it is not. I've tried to make it a bit more clever with the images so as not to send big images to small phones which get bogged down trying to render them. So the server downsizes the image if necessary. Also the server resolves all dependencies in the javascript it sends and bundles them with it so it all goes as one big file. All of this gets cached in production too of course.
+
+The javascript is commented to explain what it is doing. The css is and always will be a brainfuck so I'm not even going to bother explaining that.
+
+## Running the server locally
+
+If you have [make](http://www.gnu.org/software/make/) and [packin](http://github.com/jkroso/packin) installed you only have to run `make` in your console. Otherwise run:
+
+```sh
+npm install
+node app.js
+```
--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,33 @@
+
+/**
+ * module dependencies
+ */
+
 var bind = require('component/event').bind
 var viewport = require('jkroso/viewport')
 var Swipe = require('component/swipe')
 var flow = require('jkroso/flowtype')
 
+/**
+ * initialize a new Swipe instance. This just turns what
+ * would of been a plain list of images into a swipe-able
+ * slideshow which only shows one image at a time
+ */
+
 var swipe = new Swipe(document.getElementById('images'))
+
+/**
+ * get references to the left an right arrows shown over the
+ * top images
+ */
+
 var left = document.getElementsByTagName('svg')[0].firstChild
 var right = document.getElementsByTagName('svg')[1].firstChild
+
+/**
+ * show/hide the arrows when the swipe instance is showing
+ * the first or last image
+ */
 
 swipe.on('showing', function(){
 	if (swipe.isFirst()) {
@@ -20,7 +42,16 @@ swipe.on('showing', function(){
 	}
 })
 
+/**
+ * initially show the first image
+ */
+
 swipe.show(0)
+
+/**
+ * handle left/right keyboard keys
+ * @param {Event} e
+ */
 
 bind(window, 'keydown', function(e){
 	if (e.which == 37/*left*/)  swipe.prev()
@@ -28,14 +59,35 @@ bind(window, 'keydown', function(e){
 	if (e.which == 39 || e.which == 37) e.preventDefault()
 })
 
+/**
+ * handle clicks on the swipe arrows
+ */
+
+bind(left,  'mousedown', function(){ swipe.prev() })
+bind(right, 'mousedown', function(){ swipe.next() })
+
+/**
+ * swipe needs to know when the window changes size
+ */
+
+viewport.on('resize', function(){
+	swipe.refresh()
+})
+
+/**
+ * pause/play the video when it is clicked
+ */
+
 var video = document.querySelector('video')
 bind(video, 'click', function() {
 	if (video.paused) video.play()
 	else video.pause()
 })
 
-bind(left,  'mousedown', function(){ swipe.prev() })
-bind(right, 'mousedown', function(){ swipe.next() })
+/**
+ * initialize flow on the document. It adjusts the font-size
+ * to match the screen width
+ */
 
 flow(document.querySelector('.body'), {
 	lineRatio: 1.5,
@@ -44,6 +96,3 @@ flow(document.querySelector('.body'), {
 	max: 16
 })
 
-viewport.on('resize', function(){
-	swipe.refresh()
-})
